0a8377f4cba4c0b317b84d912bec7cebf8666296
daredevil17052004 <anshs052004@gmail.com>
2023-11-03 23:17:27 +0530

    final commit

--- promise-async.js
+++ promise-async.js
@@ -2,12 +2,35 @@ const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
 const newCookie = {name:"Biscotti Cookies"};
 
 // Progression 1: create a function to get all the cookies
+function getCookie(){
+   setTimeout(() =>{
+      let op = " ";
+      cookies.forEach((bisc) => {
+         op = op + `<li>${bisc.name}</li>`;
+      });
+      document.body.innerHTML = op;
+   },1000)
+}
+
 
    // Progression 2: using setTimeout() - use 1000 units for time parameter
 
 
 //Progression 3: Create a function to creat cookies and create an object of Promise.
 
+function makeCookie(newCookie){
+   return new Promise((resolve,reject) => {
+      setTimeout(() => {
+         cookies.push(newCookie);
+         let error = true;
+         if(error){
+            resolve();
+         } else{
+            reject(`There are no such cookies`)
+         }
+      }, 2000);
+   });
+};
 
     // Progression 4: adding two parameters and using setTimeout() - use 2000 units for time parameter
 
@@ -16,6 +39,9 @@ const newCookie = {name:"Biscotti Cookies"};
 
 
 // Progression 7: creating a new async function
-
+async function ayay(){
+   await makeCookie(newCookie);
+   getCookie();
+}
 // calling the new async function
-
+ayay();
